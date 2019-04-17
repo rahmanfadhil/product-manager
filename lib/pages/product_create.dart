3396ceee28254bed4d22dcd23390c9cce289b2cf
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-17 12:11:12 +0700

    Add modal in product create page

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -4,7 +4,19 @@ class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Create product"),
+      child: RaisedButton(
+        child: Text('Save'),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return Center(
+                child: Text('Hello'),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
