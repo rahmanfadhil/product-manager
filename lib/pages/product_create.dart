970b8b621d788da88202fc00e943703cfd2433d5
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-18 21:43:22 +0700

    Add media query to product create page

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -65,9 +65,15 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
 
   @override
   Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 550.0 ? 500 : deviceWidth * 0.95;
+    final double targetPadding = deviceWidth - targetWidth;
+
     return Container(
+      width: targetWidth,
       margin: EdgeInsets.all(10.0),
       child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
         children: <Widget>[
           _buildTitleTextField(),
           _buildDescriptionTextField(),
