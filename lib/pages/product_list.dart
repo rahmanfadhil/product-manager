735268bb4b0f7973e7534c43d6385960768cf16d
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-19 06:47:02 +0700

    Add divider to product list

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -13,23 +13,31 @@ class ProductListPage extends StatelessWidget {
     return ListView.builder(
       itemCount: products.length,
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          // leading: Image.asset(products[index]['image']),
-          title: Text(products[index]['title']),
-          trailing: IconButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) => ProductEditPage(
-                        product: products[index],
-                        updateProduct: updateProduct,
-                        productIndex: index,
-                      ),
-                ),
-              );
-            },
-            icon: Icon(Icons.edit),
-          ),
+        return Column(
+          children: <Widget>[
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(products[index]['image']),
+              ),
+              title: Text(products[index]['title']),
+              subtitle: Text('\$${products[index]['price'].toString()}'),
+              trailing: IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => ProductEditPage(
+                            product: products[index],
+                            updateProduct: updateProduct,
+                            productIndex: index,
+                          ),
+                    ),
+                  );
+                },
+                icon: Icon(Icons.edit),
+              ),
+            ),
+            Divider()
+          ],
         );
       },
     );
